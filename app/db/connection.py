f7167ec28939f8cb44b9967e5dd3642b58e5fd09
João Pedro <joao2015.76@gmail.com>
2024-06-14 17:36:18 -0300

    Added alembic for migrations

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql+psycopg2://postgres:postgres@34.95.165.153/postgres", echo=True, pool_pre_ping=True)
+engine = create_engine("postgresql+psycopg2://postgres:postgres@35.247.195.118/postgres", echo=True, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
