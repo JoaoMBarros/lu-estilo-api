843b86d6a6be09ec00ff10cfd1b41cfb1c89b9d9
João Pedro <joao2015.76@gmail.com>
2024-06-20 13:36:31 -0300

    Order refactor

--- app/order/orders_service.py
+++ app/order/orders_service.py
@@ -2,25 +2,31 @@ from fastapi.exceptions import HTTPException
 from fastapi import status
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session, joinedload
-from app.schemas import OrderBase, OrderCreate, OrderProductJoinCreate
-from app.db.models import OrderModel, ProductModel, OrderProductJoin
+from app.schemas import OrderCreate, OrderBoughtProducts, OrderProductReturn, OrderProductJoinCreate, OrderRegister, OrderDatabase, OrderRegisterReturn
+from app.db.models import OrderModel, ProductModel, OrderProductJoin, ClientModel
 from datetime import datetime
 
 class OrderService:
     def __init__(self, db: Session):
         self.db = db
     
-    def get_order_by_id(self, order_id: int):
-        return self.db.query(OrderModel).options(joinedload(OrderProductJoin)).filter(OrderModel.id == order_id).first()
+    def get_order_from_database(self, order_id: str):
+        return self.db.query(OrderModel).filter(OrderModel.id == order_id).options(joinedload(OrderModel.products)).first()
 
-    def create_order(self, order: OrderBase):
+    def create_order(self, order: OrderRegister):
         try:
+            client = self.db.query(ClientModel).filter(ClientModel.id == order.client_id).first()
+            if not client:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Client not found")
+            
             calculated_total_price = 0
             bought_products = []
             for product in order.products:
                 db_product = self.db.query(ProductModel).filter(ProductModel.id == product.product_id).first()
+                if not db_product:
+                    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Product not found")
                 calculated_total_price += db_product.price * product.quantity
-                bought_products.append(product)
+                bought_products.append(OrderBoughtProducts(**db_product.__dict__, quantity=product.quantity))
 
             if bought_products == []:
                 raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No products found")
@@ -40,11 +46,13 @@ class OrderService:
             for product in bought_products:
                 order_product_join = OrderProductJoinCreate(
                     order_id=db_order.id,
-                    product_id=product.product_id,
+                    product_id=product.id,
                     quantity=product.quantity
                 )
                 order_product = OrderProductJoin(**order_product_join.model_dump())
                 self.db.add(order_product)
+
+            return OrderRegisterReturn(client_id=db_order.client_id, status=db_order.status, total_price=db_order.total_price, id=db_order.id)
 
             self.db.commit()
         except IntegrityError:
@@ -56,72 +64,98 @@ class OrderService:
                    final_date: datetime = None,
                    status: str = None,
                    products_section: str = None,
-                   client_id: str = None
+                   client_id: str = None,
+                   page: int = 1,
+                   page_size: int = 10
                    ):
 
         if start_date and final_date:
             final_date = final_date.replace(hour=23, minute=59, second=59)
-            orders = self.db.query(OrderModel).filter(OrderModel.created_at >= start_date, OrderModel.created_at <= final_date)
+            query = self.db.query(OrderModel).filter(OrderModel.created_at >= start_date.isoformat(), OrderModel.created_at <= final_date.isoformat())
         elif status:
-            orders = self.db.query(OrderModel).filter(OrderModel.status == status)
+            query = self.db.query(OrderModel).filter(OrderModel.status == status)
         elif products_section:
-            orders = self.db.query(OrderModel).filter(ProductModel.section == products_section)
+            query = self.db.query(OrderModel).filter(ProductModel.section == products_section)
         elif client_id:
-            orders = self.db.query(OrderModel).filter(OrderModel.client_id == client_id)
+            query = self.db.query(OrderModel).filter(OrderModel.client_id == client_id)
         else:
-            orders = self.db.query(OrderModel)
-
-        # orders = orders_query.options(joinedload(OrderModel.products).joinedload(OrderProductJoin.product)).all()
+            query = self.db.query(OrderModel)
         
+        orders = query.offset((page - 1) * page_size).limit(page_size).all()
+
         final_result = []
 
         for order in orders:
-
-            final_result.append({
-                "id": order.id,
-                "user_id": order.client_id,
-                "created_at": order.created_at.isoformat(),
-                "status": order.status,
-                "products": [
-                    {
-                        "product_id": product.product_id,
-                        "name": product.product.name,
-                        "price": product.product.price,
-                        "quantity": product.quantity
-                    } for product in order.products],
-                "total": order.total_price
-            })
+            products = []
+            for product in order.products:
+                products.append(OrderProductReturn(
+                    product_id=product.product_id,
+                    name=product.product.name,
+                    price=product.product.price,
+                    quantity= product.quantity
+                ))
+            final_result.append(OrderDatabase(
+                id=order.id,
+                client_id=order.client_id,
+                created_at=order.created_at,
+                status=order.status,
+                products=products,
+                total_price=order.total_price
+            ))
 
         return final_result
 
     def get_order_by_id(self, order_id: str):
-        order = self.db.query(OrderModel).options(joinedload(OrderProductJoin)).filter(OrderModel.id == order_id).first()
+        order = self.db.query(OrderModel).filter(OrderModel.id == order_id).first()
         if order:
-            return {
-                "id": order.id,
-                "user_id": order.user_id,
-                "products": [{"product_id": product.product_id, "quantity": product.quantity} for product in order.products],
-                "total": order.total_price
-            }
+            products = []
+            for product in order.products:
+                products.append(OrderProductReturn(
+                    product_id=product.product_id,
+                    name=product.product.name,
+                    price=product.product.price,
+                    quantity= product.quantity
+                ))
+            
+            return OrderDatabase(
+                id=order.id,
+                client_id=order.client_id,
+                created_at=order.created_at,
+                status=order.status,
+                products=products,
+                total_price=order.total_price
+            )
         else:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
 
-    def update_order(self, order_id: int, order: OrderBase):
-        db_order = self.get_order_by_id(order_id)
+    def update_order(self, order_id: str, order: OrderCreate):
+        db_order = self.get_order_from_database(order_id)
 
         if db_order:
-            db_order.user_id = order.user_id
-            db_order.total_price = order.total
+            new_client_db = self.db.query(ClientModel).filter(ClientModel.id == order.client_id).first()
+            if not new_client_db:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Client not found")
+            db_order.client_id = order.client_id
+            db_order.total_price = order.total_price
             db_order.status = order.status
             self.db.commit()
             self.db.refresh(db_order)
+            updated_order = OrderCreate(
+                client_id=db_order.client_id,
+                total_price=db_order.total_price,
+                status=db_order.status
+            )
+            return updated_order
         else:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
     
     def delete_order(self, order_id: int):
-        db_order = self.get_order_by_id(order_id)
+        db_order = self.get_order_from_database(order_id)
 
         if db_order:
+            for product in db_order.products:
+                self.db.delete(product)
+            
             self.db.delete(db_order)
             self.db.commit()
         else:
--- app/order/routes.py
+++ app/order/routes.py
@@ -1,56 +1,60 @@
 from fastapi import APIRouter, Depends, status, Query
 from fastapi.responses import JSONResponse, Response
 from sqlalchemy.orm import Session
-from app.depends import get_db, token_verifier
+from app.depends import get_db, token_verifier, is_admin
 from app.order.orders_service import OrderService
-from app.schemas import OrderBase
+from app.schemas import OrderRegister, OrderCreate, OrderDatabase, OrderRegisterReturn
 from typing import Optional
 from datetime import datetime
 
-order_router = APIRouter(prefix='/orders', dependencies=[Depends(token_verifier)])
+order_router = APIRouter(prefix='/orders', dependencies=[Depends(token_verifier), Depends(is_admin)])
 
-@order_router.post("/")
-async def create_order(order: OrderBase, db: Session = Depends(get_db)):
+@order_router.post("/", response_model=OrderDatabase, responses={201: {"description": "Created"}})
+async def create_order(order: OrderRegister, db: Session = Depends(get_db)):
     order_service = OrderService(db=db)
-    order_service.create_order(order=order)
-    return Response(status_code=status.HTTP_201_CREATED)
+    order = order_service.create_order(order=order)
+    return JSONResponse(content=order.model_dump(), status_code=status.HTTP_201_CREATED)
 
-@order_router.get("/")
+@order_router.get("/", response_model=OrderDatabase)
 async def get_orders(
         start_date: Optional[datetime] = Query(None),
         final_date: Optional[datetime] = Query(None),
         order_status: Optional[str] = Query(None),
         products_section: Optional[str] = Query(None),
         client_id: Optional[str] = Query(None),
+        page: int = Query(1, gt=0),
+        page_size: int = Query(10, gt=0, le=100),
         db: Session = Depends(get_db)
     ):
+
     order_service = OrderService(db=db)
     orders = order_service.get_orders(
         start_date=start_date,
         final_date=final_date,
         status=order_status,
         products_section=products_section,
-        client_id=client_id
+        client_id=client_id,
+        page=page,
+        page_size=page_size
     )
-
     if orders:
-        return JSONResponse(content=orders, status_code=status.HTTP_200_OK)
+        return JSONResponse(content=[order.model_dump() for order in orders], status_code=status.HTTP_200_OK)
     else:
         return JSONResponse(content=[], status_code=status.HTTP_200_OK)
 
-@order_router.get("/{id}")
+@order_router.get("/{id}", response_model=OrderDatabase, responses={200: {"description": "OK"}, 404: {"description": "Not Found"}})
 async def get_order_by_id(id: str, db: Session = Depends(get_db)):
     order_service = OrderService(db=db)
     order = order_service.get_order_by_id(id)
-    return JSONResponse(content=order, status_code=status.HTTP_200_OK)
+    return JSONResponse(content=order.model_dump(), status_code=status.HTTP_200_OK)
 
-@order_router.put("/{id}")
-async def update_order(id: str, order: OrderBase, db: Session = Depends(get_db)):
+@order_router.put("/{id}", response_model=OrderDatabase, responses={200: {"description": "OK"}, 404: {"description": "Not Found"}})
+async def update_order(id: str, order: OrderCreate, db: Session = Depends(get_db)):
     order_service = OrderService(db=db)
-    order_service.update_order(order_id=id, order=order)
-    return Response(status_code=status.HTTP_200_OK)
+    order = order_service.update_order(order_id=id, order=order)
+    return JSONResponse(content=order.model_dump(), status_code=status.HTTP_200_OK)
 
-@order_router.delete("/{id}")
+@order_router.delete("/{id}", responses={204: {"description": "No Content"}, 404: {"description": "Not Found"}})
 async def delete_order(id: str, db: Session = Depends(get_db)):
     order_service = OrderService(db=db)
     order_service.delete_order(order_id=id)
