364755ba188f93e4ddbfff62b9717cf3b3de83c2
João Pedro <joao2015.76@gmail.com>
2024-06-20 13:36:53 -0300

    User refactor

--- app/user/auth_user.py
+++ app/user/auth_user.py
@@ -4,7 +4,7 @@ from fastapi import status
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from app.db.models import UserModel
-from app.schemas import UserLogin, UserCreate, UserBase
+from app.schemas import UserLogin, UserDatabase, UserRegister, UserRegisterReturn, UserCreate, UserRefreshToken
 from passlib.context import CryptContext
 from jose import JWTError, jwt
 from decouple import config
@@ -25,33 +25,39 @@ class UserCases:
     
     def create_access_token(self, data: dict):
         to_encode = data.copy()
+
+        # Set expiration time
         local_utc = pytz.timezone("America/Sao_Paulo")
         expires = datetime.datetime.now(local_utc) + datetime.timedelta(minutes=ACCESS_TOKEN_MINUTES_EXP)
         to_encode["exp"] = expires
+
         encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
         return encoded_jwt, expires.isoformat()
 
     def create_refresh_token(self, data: dict):
         to_encode = data.copy()
+
+        # Set expiration time
         local_utc = pytz.timezone("America/Sao_Paulo")
         expires = datetime.datetime.now(local_utc) + datetime.timedelta(days=REFRESH_TOKEN_DAYS_EXP)
         to_encode["exp"] = expires
+
         encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
         return encoded_jwt, expires.isoformat()
 
-    def user_register(self, user: UserBase):
+    def user_register(self, user: UserRegister):
         hashed_password = pwd_context.hash(user.password)
+
+        refresh_token, expires = self.create_refresh_token(data={"sub": user.email, "token_type": "refresh", "role": "admin"})
         
-        refresh_token, expires = self.create_refresh_token(data={"sub": user.email, "token_type": "refresh"})
-
-        # Using the UserCreate schema to validate the data
-        user = UserCreate(name=user.name, email=user.email, password=hashed_password, refresh_token=refresh_token)
+        user = UserCreate(name=user.name, email=user.email, password=hashed_password, refresh_token=refresh_token, role="admin")
         db_user = UserModel(**user.model_dump())
-
+       
         try:
             self.db.add(db_user)
             self.db.commit()
             self.db.refresh(db_user)
+            return UserRegisterReturn(id=db_user.id, name=db_user.name, email=db_user.email, password=db_user.password)
         except IntegrityError:
             self.db.rollback()
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
@@ -64,10 +70,15 @@ class UserCases:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
 
         # Access token creation
-        data = {"sub": db_user.email, "token_type": "access"}
+        data = {"sub": db_user.email, "token_type": "access", "role": db_user.role}
         access_token, expires = self.create_access_token(data)
 
-        user = UserCreate(name=db_user.name, email=db_user.email, password=db_user.password, refresh_token=db_user.refresh_token)
+        user = UserDatabase(
+                        id=db_user.id,
+                        name=db_user.name, 
+                        email=db_user.email, 
+                        password=db_user.password, 
+                        refresh_token=db_user.refresh_token)
 
         return {"access_token": access_token, "token_type": "bearer", "expires_in": expires, "user": user.model_dump()}
 
@@ -115,6 +126,13 @@ class UserCases:
             user.refresh_token = refresh_token
             self.db.commit()
             self.db.refresh(user)
-            return {"access_token": access_token, "access_expires_in": access_date_expires, "refresh_token": refresh_token, "refresh_expires_in": refresh_date_expires}
+            return UserRefreshToken(access_token= access_token, access_expires_in= access_date_expires, refresh_token= refresh_token, refresh_expires_in= refresh_date_expires)
         except JWTError:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        
+    def verify_role(self, token: str):
+        data = self.verify_access_token(token)
+        token_role = data.get('role')
+        if token_role != "admin":
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid role")
+        return token_role
--- app/user/routes.py
+++ app/user/routes.py
@@ -3,23 +3,29 @@ from fastapi.responses import JSONResponse, Response
 from sqlalchemy.orm import Session
 from app.depends import get_db
 from app.user.auth_user import UserCases
-from app.schemas import User, UserLogin, UserBase
+from app.schemas import UserLogin, UserRegister, UserDatabase, UserRegisterReturn, UserRefreshToken
 from app.depends import refresh_token_verifier
 
 user_router = APIRouter(prefix='/auth')
 
-@user_router.post("/register")
-async def create_user(user: UserBase, db: Session = Depends(get_db)):
-    user_repo = UserCases(db=db)
-    user_repo.user_register(user=user)
-    return Response(status_code=status.HTTP_201_CREATED)
+@user_router.post("/register", response_model=UserRegisterReturn, responses={400: {"description": "Bad Request"}})
+async def create_user(user: UserRegister, db: Session = Depends(get_db)):
+    try:
+        user_repo = UserCases(db=db)
+        registered_user = user_repo.user_register(user=user)
+        return JSONResponse(content=registered_user.model_dump(), status_code=status.HTTP_201_CREATED)
+    except Exception as e:
+        return JSONResponse(content=e.errors(), status_code=status.HTTP_400_BAD_REQUEST)
 
-@user_router.post("/login")
+@user_router.post("/login", 
+                  response_model=UserDatabase, 
+                  responses={404: {"description": "User not found"}, 
+                             401: {"description": "Invalid password"}})
 async def login_user(user_login: UserLogin, db: Session = Depends(get_db)):
     user_repo = UserCases(db=db)
     auth_data = user_repo.authenticate_user(user_data=user_login)
     return JSONResponse(content=auth_data, status_code=status.HTTP_200_OK)
 
-@user_router.post("/refresh-token")
-async def refresh_token(token: dict = Depends(refresh_token_verifier), db: Session = Depends(get_db)):
-    return JSONResponse(content=token, status_code=status.HTTP_200_OK)
+@user_router.post("/refresh-token", response_model=UserRefreshToken, responses={401: {"description": "Unauthorized"}})
+async def refresh_token(token: dict = Depends(refresh_token_verifier)):
+    return JSONResponse(content=token.model_dump(), status_code=status.HTTP_200_OK)
