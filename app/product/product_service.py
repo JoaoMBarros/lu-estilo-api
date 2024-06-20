e1f2e5d617da03c9a4aa7648279517c7999beb63
João Pedro <joao2015.76@gmail.com>
2024-06-20 13:36:44 -0300

    Product refactor

--- app/product/product_service.py
+++ app/product/product_service.py
@@ -3,7 +3,7 @@ from fastapi import status
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session, joinedload
 from app.db.models import ProductModel, CategoryModel, ProductImages, ProductCategoryJoin
-from app.schemas import Product, ProductBase, ProductCreate, Category, ProductImagesCreate
+from app.schemas import ProductRegister, ProductRegisterReturn, ProductCreate, ProductCategory, ProductImagesCreate, ProductImagesDatabase, ProductDatabase, ProductCategoryJoinCreate
 
 class ProductService:
     def __init__(self, db: Session):
@@ -11,33 +11,24 @@ class ProductService:
 
     def get_category_by_id(self, category_id: int):
         category = self.db.query(CategoryModel).filter(CategoryModel.id == category_id).first()
-        return Category(**category.__dict__) if category else None
+        return ProductCategory(**category.__dict__) if category else None
     
-    def create_product(self, product: ProductBase):
+    def create_product(self, product: ProductRegister):
         try:
             self.db.begin_nested()
-
-            product_instance = ProductCreate(
-                name=product.name,
-                price=product.price,
-                description=product.description,
-                barcode=product.barcode,
-                section=product.section,
-                stock=product.stock,
-                expire_date=product.expire_date,
-                available=product.available,
-            )
-
-            db_product = ProductModel(**product_instance.model_dump())
+            schema_data_product = ProductCreate(**product.model_dump())
+            db_product = ProductModel(**schema_data_product.model_dump())
             self.db.add(db_product)
             self.db.flush()
 
             for category in product.categories:
                 category_schema = self.get_category_by_id(category.id)
-                db_product_category = ProductCategoryJoin(product_id=str(db_product.id), category_id=category_schema.id)
+                if category.name != category_schema.name:
+                    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Category name does not match the id")
+                product_category_schema_data = ProductCategoryJoinCreate(product_id=str(db_product.id), category_id=category_schema.id)
+                db_product_category = ProductCategoryJoin(**product_category_schema_data.model_dump())
                 self.db.add(db_product_category)
-                self.db.flush()
-    
+
             for image in product.images:
                 image = ProductImagesCreate(
                     image_url=image.image_url,
@@ -45,62 +36,89 @@ class ProductService:
                 )
                 db_image = ProductImages(**image.model_dump())
                 self.db.add(db_image)
-                self.db.flush()
-        
+
             self.db.commit()
             self.db.refresh(db_product)
+
+            return ProductRegisterReturn(id=db_product.id, 
+                                         name=db_product.name, 
+                                         price=db_product.price, 
+                                         description=db_product.description, 
+                                         barcode=db_product.barcode, 
+                                         section=db_product.section, 
+                                         stock=db_product.stock, 
+                                         expire_date=db_product.expire_date, 
+                                         available=db_product.available,
+                                         images = product.images,
+                                         categories = product.categories)
         except IntegrityError:
             self.db.rollback()
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Product already registered")
         
-    def get_products(self, category: str = None, available: bool = None, price: int = None):
+    def get_products(self, category: str = None, available: bool = None, price: int = None, page: int = 1, page_size: int = 10):
         if category:
-            products = self.db.query(ProductModel).join(ProductModel.categories).join(CategoryModel).filter(CategoryModel.name == category).all()
+            products = self.db.query(ProductModel).join(ProductModel.categories).join(CategoryModel).filter(CategoryModel.name == category).offset((page - 1) * page_size).limit(page_size).all()
 
-        elif available:
-            products = self.db.query(ProductModel).filter(ProductModel.available == available).all()
+        elif available == True or available == False:
+            products = self.db.query(ProductModel).filter(ProductModel.available == available).offset((page - 1) * page_size).limit(page_size).all()
         
         elif price:
-            products = self.db.query(ProductModel).filter(ProductModel.price == price).all()
+            products = self.db.query(ProductModel).filter(ProductModel.price == price).offset((page - 1) * page_size).limit(page_size).all()
 
         else:
-            products = self.db.query(ProductModel).options(joinedload(ProductModel.categories)).all()
-        
+            products = self.db.query(ProductModel).options(joinedload(ProductModel.categories)).offset((page - 1) * page_size).limit(page_size).all()
+
         final_result = []
         for product in products:
-            final_result.append({
-                "id": product.id,
-                "name": product.name,
-                "price": product.price,
-                "description": product.description,
-                "barcode": product.barcode,
-                "section": product.section,
-                "stock": product.stock,
-                "expire_date": product.expire_date.isoformat(),
-                "available": product.available,
-                "categories": [{'id': category.category.id, 'name': category.category.name} for category in product.categories],
-                "images": [{'image_url': image.image_url} for image in product.images]
-            })
+            categories = []
+
+            images = [ProductImagesDatabase(id=image.id, image_url=image.image_url) for image in product.images if image]
+            
+            for category in product.categories:
+                categories.append(ProductCategory(id=category.category.id, name=category.category.name))
         
+            final_result.append(ProductDatabase(
+                id=product.id,
+                name=product.name,
+                price= product.price,
+                description= product.description,
+                barcode=product.barcode,
+                section= product.section,
+                stock=product.stock,
+                expire_date= product.expire_date,
+                available= product.available,
+                categories= categories,
+                images= images
+            ))
         return final_result
 
     def get_product_by_id(self, product_id: str):
         product = self.db.query(ProductModel).options(joinedload(ProductModel.categories)).filter(ProductModel.id == product_id).first()
-        return {
-                "id": product.id,
-                "name": product.name,
-                "price": product.price,
-                "description": product.description,
-                "barcode": product.barcode,
-                "section": product.section,
-                "stock": product.stock,
-                "expire_date": product.expire_date.isoformat(),
-                "available": product.available,
-                "categories": [{'id': category.category.id, 'name': category.category.name} for category in product.categories],
-                "images": [{'image_url': image.image_url} for image in product.images]
-            }
+        
+        if product:
+            images = [ProductImagesDatabase(id=image.id, image_url=image.image_url) for image in product.images if image]
 
-    def update_product(self, product_id: str, product: ProductBase):
+            categories = []
+            for category in product.categories:
+                categories.append(ProductCategory(id=category.category.id, name=category.category.name))
+            
+            return ProductDatabase(
+                id=product.id,
+                name=product.name,
+                price= product.price,
+                description= product.description,
+                barcode=product.barcode,
+                section= product.section,
+                stock=product.stock,
+                expire_date= product.expire_date,
+                available= product.available,
+                categories= categories,
+                images= images
+            )
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+
+    def update_product(self, product_id: str, product: ProductRegister):
         db_product = self.db.query(ProductModel).options(joinedload(ProductModel.categories)).filter(ProductModel.id == product_id).first()
         if db_product:
             db_products_categories_ids = [category.category.id for category in db_product.categories]
@@ -108,16 +126,17 @@ class ProductService:
             # Update new categories
             for category in product.categories:
                 if category.id not in db_products_categories_ids:
-                    new_join_product_category = ProductCategoryJoin(product_id=str(db_product.id), category_id=category.id)
-                    self.db.add(new_join_product_category)
-                    self.db.flush()
+                    new_join_product_category_schema = ProductCategoryJoinCreate(product_id=str(db_product.id), category_id=category.id)
+                    db_new_join_product_category = ProductCategoryJoin(**new_join_product_category_schema.model_dump())
+                    self.db.add(db_new_join_product_category)
+                    # self.db.flush()
             
             # Remove old categories
             for category_id in db_products_categories_ids:
                 if category_id not in [category.id for category in product.categories]:
                     db_product_category = self.db.query(ProductCategoryJoin).filter(ProductCategoryJoin.category_id == category_id, ProductCategoryJoin.product_id == db_product.id).first()
                     self.db.delete(db_product_category)
-                    self.db.flush()
+                    # self.db.flush()
             
             # Update new images
             for image in product.images:
@@ -128,14 +147,14 @@ class ProductService:
                     )
                     db_image = ProductImages(**image.model_dump())
                     self.db.add(db_image)
-                    self.db.flush()
+                    # self.db.flush()
             
             # Remove old images
             for image in db_product.images:
                 if image.image_url not in [image.image_url for image in product.images]:
                     db_image = self.db.query(ProductImages).filter(ProductImages.image_url == image.image_url).first()
                     self.db.delete(db_image)
-                    self.db.flush()
+                    # self.db.flush()
 
             db_product.name = product.name
             db_product.price = product.price
@@ -147,12 +166,33 @@ class ProductService:
             db_product.available = product.available
             self.db.commit()
             self.db.refresh(db_product)
+            updated_product = ProductDatabase(
+                id=db_product.id,
+                name=db_product.name,
+                price=db_product.price,
+                description=db_product.description,
+                barcode=db_product.barcode,
+                section=db_product.section,
+                stock=db_product.stock,
+                expire_date=db_product.expire_date,
+                available=db_product.available,
+                categories=[ProductCategory(id=category.category.id, name=category.category.name) for category in db_product.categories],
+                images=[ProductImagesDatabase(id=image.id, image_url=image.image_url) for image in db_product.images]
+            )
+            return updated_product
         else:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
     
     def delete_product(self, product_id: str):
-        db_product = self.db.query(ProductModel).filter(ProductModel.id == product_id).first()
+        db_product = self.db.query(ProductModel).filter(ProductModel.id == product_id).options(joinedload(ProductModel.categories)).first()
+        
         if db_product:
+            for category in db_product.categories:
+                self.db.delete(category)
+            
+            for image in db_product.images:
+                self.db.delete(image)
+
             self.db.delete(db_product)
             self.db.commit()
         else:
--- app/product/routes.py
+++ app/product/routes.py
@@ -1,50 +1,49 @@
 from fastapi import APIRouter, Depends, status, Query
 from fastapi.responses import JSONResponse, Response
 from sqlalchemy.orm import Session
-from app.depends import get_db, token_verifier
+from app.depends import get_db, token_verifier, is_admin
 from app.product.product_service import ProductService
-from app.schemas import ProductBase
+from app.schemas import ProductRegister, ProductDatabase, ProductRegisterReturn
 from typing import Optional
 
-product_router = APIRouter(prefix='/products', dependencies=[Depends(token_verifier)])
+product_router = APIRouter(prefix='/products', dependencies=[Depends(token_verifier), Depends(is_admin)])
 
-@product_router.post("/")
-async def create_product(product: ProductBase, db: Session = Depends(get_db)):
+@product_router.post("/", response_model=ProductDatabase, responses={201: {"description": "Created"}})
+async def create_product(product: ProductRegister, db: Session = Depends(get_db)):
     product_service = ProductService(db=db)
-    product_service.create_product(product=product)
-    return Response(status_code=status.HTTP_201_CREATED)
+    product = product_service.create_product(product=product)
+    return JSONResponse(content=product.model_dump(), status_code=status.HTTP_201_CREATED)
 
-@product_router.get("/")
+@product_router.get("/", response_model=ProductDatabase)
 async def get_products(
     available: Optional[bool] = Query(None), 
     category: Optional[str] = Query(None),
     price: Optional[int] = Query(None),
+    page: int = Query(1, gt=0),
+    page_size: int = Query(10, gt=0, le=100),
     db: Session = Depends(get_db)
     ):
 
     product_service = ProductService(db=db)
-    products = product_service.get_products(available=available, category=category, price=price)
+    products = product_service.get_products(available=available, category=category, price=price, page=page, page_size=page_size)
     if products:
-        return JSONResponse(content=products, status_code=status.HTTP_200_OK)
+        return JSONResponse(content=[product.model_dump() for product in products], status_code=status.HTTP_200_OK)
     else:
-        return Response(status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(content=[], status_code=status.HTTP_404_NOT_FOUND)
 
-@product_router.get("/{id}")
+@product_router.get("/{id}", response_model=ProductDatabase, responses={200: {"description": "OK"}, 404: {"description": "Not Found"}})
 async def get_product_by_id(id: str, db: Session = Depends(get_db)):
     product_service = ProductService(db=db)
     product = product_service.get_product_by_id(id)
-    if product:
-        return JSONResponse(content=product, status_code=status.HTTP_200_OK)
-    else:
-        return Response(status_code=status.HTTP_404_NOT_FOUND)
+    return JSONResponse(content=product.model_dump(), status_code=status.HTTP_200_OK)
 
-@product_router.put("/{id}")
-async def update_product(id: str, product: ProductBase, db: Session = Depends(get_db)):
+@product_router.put("/{id}", response_model=ProductDatabase, responses={200: {"description": "OK"}, 404: {"description": "Not Found"}})
+async def update_product(id: str, product: ProductRegister, db: Session = Depends(get_db)):
     product_service = ProductService(db=db)
-    product_service.update_product(product_id=id, product=product)
-    return Response(status_code=status.HTTP_200_OK)
+    product = product_service.update_product(product_id=id, product=product)
+    return JSONResponse(content=product.model_dump(), status_code=status.HTTP_200_OK)
 
-@product_router.delete("/{id}")
+@product_router.delete("/{id}", responses={204: {"description": "No Content"}, 404: {"description": "Not Found"}})
 async def delete_product(id: str, db: Session = Depends(get_db)):
     product_service = ProductService(db=db)
     product_service.delete_product(product_id=id)
