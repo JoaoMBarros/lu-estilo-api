5e3730ced0e234eae8474563c62b89a0c07e1a04
João Pedro <joao2015.76@gmail.com>
2024-06-20 13:36:08 -0300

    Category refactor

--- app/category/category_service.py
+++ app/category/category_service.py
@@ -3,17 +3,16 @@ from fastapi import status
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session, joinedload
 from app.db.models import CategoryModel, ProductCategoryJoin, ProductModel
-from app.schemas import CategoryBase, CategoryCreate
+from app.schemas import CategoryRegister, CategoryCreateReturn, CategoryCreate, CategoryDatabase, CategoryProducts, CategoryProductsImages
 
 class CategoryService:
     def __init__(self, db: Session):
         self.db = db
     
-    def get_category_by_id(self, category_id: int):
+    def get_category_from_db(self, category_id: int):
         return self.db.query(CategoryModel).filter(CategoryModel.id == category_id).first()
 
-    def create_category(self, category: CategoryBase):
-        # Using the CategoryCreate schema to validate the data
+    def create_category(self, category: CategoryRegister):
         category_data = CategoryCreate(name=category.name)
         db_category = CategoryModel(**category_data.model_dump())
 
@@ -21,56 +20,97 @@ class CategoryService:
             self.db.add(db_category)
             self.db.commit()
             self.db.refresh(db_category)
+            return CategoryCreateReturn(id=db_category.id, name=db_category.name)
         except IntegrityError:
             self.db.rollback()
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Category already registered")
 
-    def get_categories(self):
+    def get_categories(self, page: int = 1, page_size: int = 10):
         categories = self.db.query(CategoryModel).options(
                 joinedload(
-                    CategoryModel.products, innerjoin=True
+                    CategoryModel.products,
                 ).joinedload(
-                    ProductCategoryJoin.product, innerjoin=True
+                    ProductCategoryJoin.product,
                 ).joinedload(
-                    ProductModel.images)).all()
-        
+                    ProductModel.images)).offset((page - 1) * page_size).limit(page_size).all()
         final_result = []
-
         for category in categories:
+            products = []
+            if category.products:
+                for pc in category.products:
+                    images = [CategoryProductsImages(id=image.id, 
+                                                    image_url=image.image_url) 
+                                                    for image in pc.product.images if image]
+                    products.append(CategoryProducts(
+                        id=pc.product.id,
+                        name=pc.product.name,
+                        price=pc.product.price,
+                        description=pc.product.description,
+                        barcode=pc.product.barcode,
+                        section=pc.product.section,
+                        stock=pc.product.stock,
+                        expire_date=pc.product.expire_date,
+                        available=pc.product.available,
+                        images=images
+                    ))
 
-            final_result.append({
-                "id": category.id,
-                "name": category.name,
-                "products": [
-                    {
-                        "id": product_category.product.id,
-                        "name": product_category.product.name,
-                        "price": product_category.product.price,
-                        "description": product_category.product.description,
-                        "barcode": product_category.product.barcode,
-                        "section": product_category.product.section,
-                        "stock": product_category.product.stock,
-                        "expire_date": product_category.product.expire_date.isoformat(),
-                        "available": product_category.product.available,
-                        "images": [{'image_url': image.image_url} for image in product_category.product.images]
-                    } for product_category in category.products
-                ]
-            })
-
+            final_result.append(CategoryDatabase(
+                id=category.id,
+                name=category.name,
+                products=products
+            ))
         return final_result
     
-    def update_category(self, category_id: int, category: CategoryBase):
-        db_category = self.get_category_by_id(category_id)
+    def get_category_by_id(self, category_id: str):
+        category = self.db.query(CategoryModel).filter(CategoryModel.id == category_id).options(
+                joinedload(
+                    CategoryModel.products
+                ).joinedload(
+                    ProductCategoryJoin.product
+                ).joinedload(
+                    ProductModel.images)).first()
+        if category:
+            products = []
+            if category.products:
+                for pc in category.products:
+                    images = [CategoryProductsImages(id=image.id, 
+                                                    image_url=image.image_url) 
+                                                    for image in pc.product.images if image]
+                    products.append(CategoryProducts(
+                        id=pc.product.id,
+                        name=pc.product.name,
+                        price=pc.product.price,
+                        description=pc.product.description,
+                        barcode=pc.product.barcode,
+                        section=pc.product.section,
+                        stock=pc.product.stock,
+                        expire_date=pc.product.expire_date,
+                        available=pc.product.available,
+                        images=images if images else []
+                    ))
+
+            return CategoryDatabase(
+                id=category.id,
+                name=category.name,
+                products=products if products else []
+            )
+        
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
+
+    def update_category(self, category_id: int, category: CategoryRegister):
+        db_category = self.get_category_from_db(category_id)
 
         if db_category:
             db_category.name = category.name
             self.db.commit()
             self.db.refresh(db_category)
+            return CategoryCreateReturn(id=db_category.id, name=db_category.name)
         else:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
 
     def delete_category(self, category_id: int):
-        db_category = self.get_category_by_id(category_id)
+        db_category = self.get_category_from_db(category_id)
 
         if db_category:
             self.db.delete(db_category)
--- app/category/routes.py
+++ app/category/routes.py
@@ -1,34 +1,40 @@
 from fastapi import APIRouter, Depends, status, Query
 from fastapi.responses import JSONResponse, Response
 from sqlalchemy.orm import Session
-from app.depends import get_db, token_verifier
+from app.depends import get_db, token_verifier, is_admin
 from app.category.category_service import CategoryService
-from app.schemas import CategoryBase
+from app.schemas import CategoryRegister, CategoryCreateReturn, CategoryDatabase
 
-category_router = APIRouter(prefix='/categories', dependencies=[Depends(token_verifier)])
+category_router = APIRouter(prefix='/categories', dependencies=[Depends(token_verifier), Depends(is_admin)])
 
-@category_router.post("/")
-async def create_category(category: CategoryBase, db: Session = Depends(get_db)):
+@category_router.post("/", response_model=CategoryCreateReturn, responses={201: {"description": "Created"}})
+async def create_category(category: CategoryRegister, db: Session = Depends(get_db)):
     category_service = CategoryService(db=db)
-    category_service.create_category(category=category)
-    return Response(status_code=status.HTTP_201_CREATED)
+    category = category_service.create_category(category=category)
+    return JSONResponse(content=category.model_dump(), status_code=status.HTTP_201_CREATED)
 
-@category_router.get("/")
-async def get_categories(db: Session = Depends(get_db)):
+@category_router.get("/", response_model=CategoryDatabase)
+async def get_categories(page: int = Query(1, gt=0), page_size: int = Query(10, gt=0, le=100),db: Session = Depends(get_db)):
     category_service = CategoryService(db=db)
-    categories = category_service.get_categories()
+    categories = category_service.get_categories(page=page, page_size=page_size)
     if categories:
-        return JSONResponse(content=categories, status_code=status.HTTP_200_OK)
+        return JSONResponse(content=[category.model_dump() for category in categories], status_code=status.HTTP_200_OK)
     else:
-        return Response(status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(content=[], status_code=status.HTTP_200_OK)
 
-@category_router.put("/{category_id}")
-async def update_category(category_id: str, category: CategoryBase, db: Session = Depends(get_db)):
+@category_router.get("/{category_id}", response_model=CategoryDatabase, responses={200: {"description": "OK"}, 404: {"description": "Not Found"}})
+async def get_category(category_id: str, db: Session = Depends(get_db)):
     category_service = CategoryService(db=db)
-    category_service.update_category(category_id=category_id, category=category)
-    return Response(status_code=status.HTTP_200_OK)
+    category = category_service.get_category_by_id(category_id=category_id)
+    return JSONResponse(content=category.model_dump(), status_code=status.HTTP_200_OK)
 
-@category_router.delete("/{category_id}")
+@category_router.put("/{category_id}", response_model=CategoryCreateReturn, responses={200: {"description": "OK"}, 404: {"description": "Not Found"}})
+async def update_category(category_id: str, category: CategoryRegister, db: Session = Depends(get_db)):
+    category_service = CategoryService(db=db)
+    category = category_service.update_category(category_id=category_id, category=category)
+    return JSONResponse(content=category.model_dump(), status_code=status.HTTP_200_OK)
+
+@category_router.delete("/{category_id}", responses={204: {"description": "No Content"}, 404: {"description": "Not Found"}})
 async def delete_category(category_id: str, db: Session = Depends(get_db)):
     category_service = CategoryService(db=db)
     category_service.delete_category(category_id=category_id)
