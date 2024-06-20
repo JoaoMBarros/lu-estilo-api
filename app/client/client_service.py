bcfd6e10d82f9c25ac0e66883ea764eb036213c4
João Pedro <joao2015.76@gmail.com>
2024-06-20 13:36:17 -0300

    Client refactor

--- app/client/client_service.py
+++ app/client/client_service.py
@@ -3,8 +3,8 @@ from fastapi.exceptions import HTTPException
 from fastapi import status
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session, joinedload
-from app.db.models import ClientModel, OrderModel
-from app.schemas import Client, ClientBase, ClientCreate
+from app.db.models import ClientModel
+from app.schemas import ClientCreate, ClientRegister, ClientCreate, ClientDatabase, ClientOrder, ClientReturnUpdate
 
 class ClientService():
     def __init__(self, db: Session):
@@ -13,7 +13,7 @@ class ClientService():
     def get_by_cpf(self, cpf: str):
         return self.db.query(ClientModel).filter(ClientModel.cpf == cpf).first()
 
-    def create_client(self, client: ClientBase):
+    def create_client(self, client: ClientRegister):
         client = ClientCreate(name=client.name, email=client.email, cpf=client.cpf)
         db_client = ClientModel(**client.model_dump())
 
@@ -21,34 +21,52 @@ class ClientService():
             self.db.add(db_client)
             self.db.commit()
             self.db.refresh(db_client)
+            created_client = ClientDatabase(
+                id=db_client.id,
+                name=db_client.name,
+                email=db_client.email,
+                cpf=db_client.cpf,
+                orders=[]
+            )
+            return created_client
         except IntegrityError:
             self.db.rollback()
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email or cpf already registered")
         
-    def get_clients(self, name = None, email = None):
+    def get_clients(self, name = None, email = None, page: int = 1, page_size: int = 10):
         query = self.db.query(ClientModel)
         if name or email:
-            query = query.filter(ClientModel.name == name) if name else query.filter(ClientModel.email == email)
-        db_clients = query.options(joinedload(ClientModel.orders)).all()
-        return [({
-            "id": client.id,
-            "name": client.name,
-            "email": client.email,
-            "cpf": client.cpf,
-            "orders": [
-                {
-                    'order_id': order.id,
-                    'created_at': order.created_at.isoformat(),
-                    'status': order.status,
-                    'total_price': order.total_price
-                } for order in client.orders]
-        }) for client in db_clients]
+            query = query.filter(ClientModel.name.like(f'%{name}%')) if name else query.filter(ClientModel.email.like(f'%{email}%'))
+        db_clients = query.options(joinedload(ClientModel.orders)).offset((page - 1) * page_size).limit(page_size).all()
+        return [ClientDatabase(
+            id=client.id,
+            name=client.name,
+            email=client.email,
+            cpf=client.cpf,
+            orders=[ClientOrder(
+                    id=order.id,
+                    created_at=order.created_at,
+                    status=order.status,
+                    total_price=order.total_price
+                    ) for order in client.orders]
+            ) for client in db_clients]
 
     def get_client_by_id(self, id: str):
         db_client = self.db.query(ClientModel).filter(ClientModel.id == id).first()
-        return Client(**db_client.__dict__) if db_client else None
+        return ClientDatabase(
+            id=db_client.id,
+            name=db_client.name,
+            email=db_client.email,
+            cpf=db_client.cpf,
+            orders=[ClientOrder(
+                    id=order.id,
+                    created_at=order.created_at,
+                    status=order.status,
+                    total_price=order.total_price
+                    ) for order in db_client.orders]
+        ) if db_client else None
     
-    def update_client(self, id: str, client: ClientBase):
+    def update_client(self, id: str, client: ClientRegister):
         db_client = self.db.query(ClientModel).filter(ClientModel.id == id).first()
         if db_client:
             db_client.name = client.name
@@ -56,6 +74,13 @@ class ClientService():
             db_client.cpf = client.cpf
             self.db.commit()
             self.db.refresh(db_client)
+
+            return ClientReturnUpdate(
+                id=db_client.id,
+                name=db_client.name,
+                email=db_client.email,
+                cpf=db_client.cpf
+            )
         else:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Client not found")
         
--- app/client/routes.py
+++ app/client/routes.py
@@ -1,45 +1,45 @@
 from fastapi import APIRouter, Depends, status, Query
 from fastapi.responses import JSONResponse, Response
 from sqlalchemy.orm import Session
-from app.depends import get_db, token_verifier
-from app.client.client_service import ClientService
-from app.schemas import ClientBase
+from app.depends import get_db, token_verifier, is_admin
+from app.client.client_service import ClientService, ClientDatabase, ClientReturnUpdate
+from app.schemas import ClientRegister
 from typing import Optional
 
 
-client_router = APIRouter(prefix='/clients', dependencies=[Depends(token_verifier)])
+client_router = APIRouter(prefix='/clients', dependencies=[Depends(token_verifier), Depends(is_admin)])
 
-@client_router.post("/")
-async def create_client(client: ClientBase, db: Session = Depends(get_db)):
+@client_router.post("/", response_model=ClientDatabase, responses={201: {"description": "Created"}})
+async def create_client(client: ClientRegister, db: Session = Depends(get_db)):
     client_service = ClientService(db=db)
-    client_service.create_client(client=client)
-    return Response(status_code=status.HTTP_201_CREATED)
+    client = client_service.create_client(client=client)
+    return JSONResponse(content=client.model_dump(), status_code=status.HTTP_201_CREATED)
 
-@client_router.get("/")
-async def get_clients(name: Optional[str] = Query(None), email: Optional[str] = Query(None), db: Session = Depends(get_db)):
+@client_router.get("/", response_model=ClientDatabase)
+async def get_clients(name: Optional[str] = Query(None), email: Optional[str] = Query(None), page: int = Query(1, gt=0), page_size: int = Query(10, gt=0, le=100), db: Session = Depends(get_db)):
     client_service = ClientService(db=db)
-    clients = client_service.get_clients(name = name, email = email)
+    clients = client_service.get_clients(name = name, email = email, page = page, page_size = page_size)
     if clients:
-        return JSONResponse(content=clients, status_code=status.HTTP_200_OK)
+        return JSONResponse(content=[client.model_dump() for client in clients], status_code=status.HTTP_200_OK)
     else:
-        return Response(status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(content=[], status_code=status.HTTP_200_OK)
     
-@client_router.get("/{id}")
+@client_router.get("/{id}", response_model=ClientDatabase, responses={200: {"description": "OK"}, 404: {"description": "Not Found"}})
 async def get_client(id: str, db: Session = Depends(get_db)):
     client_service = ClientService(db=db)
     client = client_service.get_client_by_id(id)
     if client:
-        return Response(content=client.model_dump(), status_code=status.HTTP_200_OK)
+        return JSONResponse(content=client.model_dump(), status_code=status.HTTP_200_OK)
     else:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
 
-@client_router.put("/{id}")
-async def update_client(id: str, client: ClientBase, db: Session = Depends(get_db)):
+@client_router.put("/{id}", response_model=ClientReturnUpdate, responses={200: {"description": "OK"}, 404: {"description": "Not Found"}})
+async def update_client(id: str, client: ClientRegister, db: Session = Depends(get_db)):
     client_service = ClientService(db=db)
-    client_service.update_client(id=id, client=client)
-    return Response(status_code=status.HTTP_200_OK)
+    client = client_service.update_client(id=id, client=client)
+    return JSONResponse(content=client.model_dump(), status_code=status.HTTP_200_OK)
 
-@client_router.delete("/{id}")
+@client_router.delete("/{id}", responses={204: {"description": "No Content"}, 404: {"description": "Not Found"}})
 async def delete_client(id: str, db: Session = Depends(get_db)):
     client_service = ClientService(db=db)
     client_service.delete_client(id=id)
